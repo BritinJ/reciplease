d144758e43fc14f6cf875f5ae2eecf157d0c55bc
Britiney Johnson <britineyjohnson@gmail.com>
2024-04-11 17:18:15 -0400

    updated all of the listcontrollers api commands

--- reciplease BE/controllers/listControllers.js
+++ reciplease BE/controllers/listControllers.js
@@ -1,12 +1,27 @@
-import { response } from 'express';
+import mongoose from 'mongoose';
 import List from '../models/List.js';
 
 export const getAllLists = async (req,res) =>{
-    res.json({msg: 'get all lists'})
+    try {
+        const lists = await List.find({}).sort({ createdAt: -1});
+        res.status(200).json(lists);
+    } catch (error) {
+        res.status(404).json({error: err.message });
+    }
 };
 
 export const getList = async (req,res) =>{
-    res.json({msg: 'get specific list'})
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error: 'list does not exist'});
+    try{
+        const list = await List.findById(id);
+        if(!list) return res.status(404).json({error: 'list does not exist'});
+        res.status(200).json(list);
+
+    }catch(err){
+        res.status(400).json({error: err.message});
+    }
 };
 
 export const createList = async (req,res) =>{
@@ -21,9 +36,35 @@ export const createList = async (req,res) =>{
 };
 
 export const deleteList = async (req,res) =>{
-    res.json({msg: 'delete a specific list'})
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error: 'list does not exist'});
+    
+    try{
+        const list = await List.findById(id);
+        if(!list) return res.status(404).json({error: 'list does not exist'});
+        
+        const deletedList = await List.findOneAndDelete({_id: id});
+        res.status(200).json(deletedList);
+
+    }catch(err){
+        res.status(400).json({error: err.message});
+    }
 };
             
 export const updateList = async (req,res) =>{
-    res.json({msg: 'update a specific list'})
+    const {id} = req.params;
+
+    if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).json({error: 'list does not exist'});
+    
+    try{
+        const list = await List.findById(id);
+        if(!list) return res.status(404).json({error: 'list does not exist'});
+        
+        const updatedList = await List.findByIdAndUpdate({_id: id}, {...req.body});
+        res.status(200).json(updatedList);
+
+    }catch(err){
+        res.status(400).json({error: err.message});
+    }
 };
